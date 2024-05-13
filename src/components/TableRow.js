ab6450104723bad65b52a1fe7babdae32ba7d14c
Rakesh Katti <rakesh.katti@gmail.com>
2024-05-13 23:03:41 +0530

    after gt

--- src/components/TableRow.js
+++ src/components/TableRow.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const qualifiedTeams = ["kkr"];
-const eliminatedTeams = ["mi", "pbks"];
+const eliminatedTeams = ["mi", "pbks", "gt"];
 const getPosition = (team, position) => {
 	if (eliminatedTeams.includes(team)) {
 		return <span className="eliminated">E</span>;
